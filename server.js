b9449daa7c8a5353117cfeee21ea3507b0507468
Mukesh T P <mukeshtp@Mukeshs-MacBook-Pro.local>
2024-10-24 21:37:52 +0530

    Updated Node.js app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js App!');
+  res.send('Hello from Node.js App!....');
 });
 
 app.listen(PORT, () => {
